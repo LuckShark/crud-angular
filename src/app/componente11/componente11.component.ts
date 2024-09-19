00bde8c20bccba6e94bb6438680012073a9098a8
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-09-19 15:57:06 -0300

    form and validators added

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
 export class Componente11Component {
-
-  nome:string;
-
-  idade:number = null;
-
+  //Objeto de Formulário
+  formulario666 = new FormGroup({
+    nome   : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade  : new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade : new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 }
+// Se quiser fazer mais de uma verificação, tem que colocar em [], representando um vetor
